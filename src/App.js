c3ce83eb6132ae3e178647fde60dc7edb3fa7a4c
elepantight <arvid1337@gmail.com>
2021-04-21 16:57:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="text-header">
+        
+      <section className="icon-list">
+              <div>
+                <a
+                  href="https://www.instagram.com/wavyoven/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="instagram"
+                >
+                <i className="nes-icon instagram is-large"></i></a>
+              </div>
+              </section>
+      </div>
+      <div className="container">
+        <div className="row">
+          <section className="message -left">
+            <div className="nes-balloon from-left">
+              <p>Welcome to Metanoia</p>
+            </div>
+          </section>
+          <section className="message -right">
+            <div className="nes-balloon from-right">
+              <p>
+                We're currently working on creating our new website.
+                <br></br>
+                We'll be launching soon, subscribe to be notified{" "}
+              </p>
+            </div>
+          </section>
+
+          <div className="nes-field">
+            <input
+              type="text"
+              name="name"
+              className="nes-input"
+              placeholder="E-mail"
+            />
+            <button type="button" className="nes-btn is-success">
+              Submit
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
