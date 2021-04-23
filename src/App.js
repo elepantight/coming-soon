cefabc3eae3e4f8328743ac8fb34b53013904f2b
elepantight <arvid1337@gmail.com>
2021-04-23 21:08:57 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from "react";
 
+import emailjs from "emailjs-com";
 
-
-import ContactUs from './components/contact';
-
-import LOGO from "./images/text_black.png";
+import LOGO from "./images/Mlogo.png";
 import "./scss/styles.scss";
 
-
-
-
 function App() {
+  function sendEmail(e) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_test",
+        e.target,
+        "user_nzRUlAOVwi3RNtg0sXepQ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <>
       <div className="text-header">
         <img className="logo-img" src={LOGO} alt="img" />
- 
       </div>
       <div className="container">
         <div className="row">
@@ -25,17 +38,35 @@ function App() {
               <p>Welcome to Metanoia</p>
             </div>
           </section>
-          <section className="container-box-2">
-            <div className="">
-              <p>
-                We're currently working on creating <br></br>our new website.
-                We'll be launching soon<br></br> subscribe to be notified{" "}
-              </p>
+          <section>
+        <div className="contact-form" onSubmit={sendEmail}>
+          <form action="index.html">
+            <div className="input-container">
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label htmlFor=""></label>
+              <span></span>
             </div>
+            <button type="submit" className="btn">
+              <span className="label">Send</span>
+              <span className="icon"></span>
+            </button>
+          </form>
+        </div>
+      </section>
+          <section className="container-box-2">
+            <p>
+              We're currently working on creating <br></br>our new website.
+              We'll be launching soon<br></br> subscribe to be notified{" "}
+            </p>
           </section>
         </div>
       </div>
-<ContactUs />
+     
       <div className="fire-1">
         <div className="flames-1">
           <div className="flame-1"></div>
@@ -56,6 +87,6 @@ function App() {
       </div>
     </>
   );
-  }
+}
 
-  export default App;
+export default App;
